0d5b63c34b5d7dd4eaa3739cc13ddc0bf8a1a484
Philippe <pvlerick@gmail.com>
2019-09-25 11:20:31 +0200

    Solution structure created

---
+++ src/HttpRequestInspector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HttpRequestInspector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
